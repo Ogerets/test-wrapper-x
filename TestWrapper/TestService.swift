472433171212981dab9601e9ec62f92e46ff2d2b
Yevhen Pyvovarov <evgenypyvovarov@gmail.com>
2021-07-10 20:19:28 +0300

    Added empty project with dependency

---
+++ TestWrapper/TestService.swift
@@ -0,0 +1,15 @@
+//
+//  TestService.swift
+//  TestWrapper
+//
+//  Created by Yevhen Pyvovarov on 10.07.2021.
+//
+
+import Foundation
+import VSCCommon
+
+class TestService {
+    func test() {
+        print("Test success")
+    }
+}
---
+++ TestWrapper/TestWrapper.h
@@ -0,0 +1,18 @@
+//
+//  TestWrapper.h
+//  TestWrapper
+//
+//  Created by Yevhen Pyvovarov on 10.07.2021.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for TestWrapper.
+FOUNDATION_EXPORT double TestWrapperVersionNumber;
+
+//! Project version string for TestWrapper.
+FOUNDATION_EXPORT const unsigned char TestWrapperVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TestWrapper/PublicHeader.h>
+
+
